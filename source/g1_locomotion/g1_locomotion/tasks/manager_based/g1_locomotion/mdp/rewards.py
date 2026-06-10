5c17481f3cb57c1fc3d0845afa151266d58bfd60
qasob <qasobovgholib@gmail.com>
2026-06-10 14:32:58 +0500

    returned missing reward mdp

--- source/g1_locomotion/g1_locomotion/tasks/manager_based/g1_locomotion/mdp/rewards.py
+++ source/g1_locomotion/g1_locomotion/tasks/manager_based/g1_locomotion/mdp/rewards.py
@@ -22,6 +22,16 @@ if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedRLEnv
 
 
+def joint_pos_target_l2(env: ManagerBasedRLEnv, target: float, asset_cfg: SceneEntityCfg) -> torch.Tensor:
+    """Penalize joint position deviation from a target value."""
+    # extract the used quantities (to enable type-hinting)
+    asset: Articulation = env.scene[asset_cfg.name]
+    # wrap the joint positions to (-pi, pi)
+    joint_pos = wrap_to_pi(asset.data.joint_pos[:, asset_cfg.joint_ids])
+    # compute the reward
+    return torch.sum(torch.square(joint_pos - target), dim=1)
+
+
 def feet_air_time_positive_biped(env, command_name: str, threshold: float, sensor_cfg: SceneEntityCfg) -> torch.Tensor:
     """Reward long steps taken by the feet for humanoid"""
 
