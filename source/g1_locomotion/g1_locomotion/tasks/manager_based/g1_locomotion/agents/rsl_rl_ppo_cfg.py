9ea298eb60c74edced48c4c32a7f3fbcc7954637
qasob <qasobovgholib@gmail.com>
2026-06-10 14:19:45 +0500

    updated ppo configs

--- source/g1_locomotion/g1_locomotion/tasks/manager_based/g1_locomotion/agents/rsl_rl_ppo_cfg.py
+++ source/g1_locomotion/g1_locomotion/tasks/manager_based/g1_locomotion/agents/rsl_rl_ppo_cfg.py
@@ -10,16 +10,16 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, R
 
 @configclass
 class PPORunnerCfg(RslRlOnPolicyRunnerCfg):
-    num_steps_per_env = 16
-    max_iterations = 150
+    num_steps_per_env = 24
+    max_iterations = 3000
     save_interval = 50
-    experiment_name = "cartpole_direct"
+    experiment_name = "g1_locomotion_ppo"
     policy = RslRlPpoActorCriticCfg(
         init_noise_std=1.0,
         actor_obs_normalization=False,
         critic_obs_normalization=False,
-        actor_hidden_dims=[32, 32],
-        critic_hidden_dims=[32, 32],
+        actor_hidden_dims=[256, 128, 128],
+        critic_hidden_dims=[256, 128, 128],
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
